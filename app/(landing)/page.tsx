297ada10cce382eecadb205a20de26f4b317b8a6
ariojuniartow <ariojuniartow@gmail.com>
2026-01-21 16:37:25 +0700

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,14 @@
+import CategoriesSection from "./components/home/categories";
+import HeroSection from "./components/home/hero";
+import ProductSection from "./components/home/products";
+
+
+export default function Home() {
+  return (
+   <main>
+    <HeroSection/>
+    <CategoriesSection/>
+    <ProductSection/>
+   </main>
+  );
+}
